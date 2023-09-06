d225a420ccf65c7355eb3da049f0a0099631bf40
seokkuuu <seokkub@gmail.com>
2023-09-06 00:50:40 -0400

    feat(auto-docs): added framework logo into appbar

--- docs/src/components/AppBar.tsx
+++ docs/src/components/AppBar.tsx
@@ -3,7 +3,10 @@ export function AppBar() {
     <div>
       <div className="w-full h-16 bg-gray-950 fixed backdrop-blur-sm bg-opacity-60 top-0 flex items-center px-2">
         <div className="container mx-auto flex items-center justify-between">
-          <h2 className="font-bold text-2xl text-fuchsia-100">AstralJS</h2>
+          <h2 className="font-bold text-2xl text-fuchsia-100 flex items-center gap-1">
+            <img src="/favicon.ico" className="h-8" />
+            AstralJS
+          </h2>
           <a
             href="https://github.com/seokkuuu/AstralJS/wiki/Getting-Started"
             target="_blank"
